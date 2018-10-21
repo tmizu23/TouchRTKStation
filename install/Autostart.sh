c03262195355850ad065cd1f67efb6fe3dab3877
Takayuki Mizutani <mizutani.takayuki@gmail.com>
2018-10-21 18:59:29 +0900

    fix typo

--- install/Autostart.sh
+++ install/Autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd /home/pi/
 mkdir .config/autostart
-cp ./TouchRTKStation/install/TouchRTKStatoinpy.desktop ./
+cp ./TouchRTKStation/install/TouchRTKStationpy.desktop ./
 cp ./TouchRTKStation/install/TouchRTKStationpy.desktop ./.config/autostart/
 chmod +x ./.config/autostart/TouchRTKStationpy.desktop
 
